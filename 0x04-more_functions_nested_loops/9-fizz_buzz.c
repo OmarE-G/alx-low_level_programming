89dc35febf2b3b272e09476646c486b3b4e8285f
Omar Essam <omar161157@eng.psu.edu.eg>
2023-05-26 07:35:42 +0300

    new commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 /**
  * fizz_buzz - prints the numbers from 1 to 100, followed by a new line.
  * But for multiples of three print Fizz instead of the number and for the
@@ -17,20 +18,22 @@ void fizz_buzz(void)
 	{
 		if ((i % 3 == 0) && (i % 5 != 0))
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if ((i % 5 == 0) && (i % 3 != 0))
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
+		if (i < 100)
+			printf(" ");
 	}
 	printf("\n");
 }
