03029adac6deb72664b3e0f9bc5fd6f355820f89
Omar Essam <omar161157@eng.psu.edu.eg>
2023-05-26 05:36:32 +0300

    new commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,15 +3,15 @@
 
 /**
  * _isdigit - checks for a digit (0 through 9)
- * 
- * @c : number to be checked 
- * Return: 1 if c is a digit, 0 otherwise 
+ *
+ * @c : number to be checked
+ * Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
 {
-    if (c >= 48 && c <= 57)
+	if (c >= 48 && c <= 57)
 
-        return (1);
-    else
-        return (0);
+		return (1);
+	else
+		return (0);
 }
