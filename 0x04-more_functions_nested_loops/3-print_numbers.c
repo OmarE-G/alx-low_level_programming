8d42ca1b1b12ab1626788d14afe3ae04e13de72c
Omar Essam <omar161157@eng.psu.edu.eg>
2023-05-26 05:41:31 +0300

    new commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,4 @@
 #include<stdio.h>
 
 
-int mul(int a, int b);
+void print_numbers(void);
