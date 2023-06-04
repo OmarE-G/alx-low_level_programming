c2a22b23295ba07572b20d4ca5c5abb8ac27d307
Omar Essam <omar161157@eng.psu.edu.eg>
2023-06-04 04:17:03 +0300

    new commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ * Description: Write a program that prints the
+ * alphabet in lowercase, and then in uppercase, followed by a new line.
+ * You can only use the putchar function
+ * (every other function (printf, puts, etc…) is forbidden)
+ * All your code should be in the main function
+ * You can only use putchar three times in your code
+ */
+int main(void)
+{
+	/*code starts here*/
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	for (i = 65; i < 91; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');	
+	return (0);
+}
+
