acb1f6e92a560f2323540197b846994d15c13edd
Omar Essam <omar161157@eng.psu.edu.eg>
2023-06-04 04:57:53 +0300

    new commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ * Description:
+ * This file must test that the function positive_or_negative()
+ * gives the correct output when given a case of 0.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*code starts here*/
+	positive_or_negative(0);
+	return (0);
+}
+
