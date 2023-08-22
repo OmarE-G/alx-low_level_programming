ebae9e53f58813207950347449de5a6a00316854
Omar Essam <omar161157@eng.psu.edu.eg>
2023-08-22 14:21:23 +0300

    new commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+
+/**
+ * print_rev - reverses string
+ *
+ * @s: string input
+ */
+
+void print_rev(char *s)
+{
+	char *e = s;
+
+	while (*e != '\0')
+		e++;
+	e--;
+	while (e != s)
+		putchar(*e--);
+	putchar(*e);
+	putchar('\n');
+}
