680a741f94c9a2bd08e66729f3106a2456a801bb
Omar Essam <omar161157@eng.psu.edu.eg>
2023-08-22 16:53:46 +0300

    new commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+/**
+*   reset_to_98 - sets val to 98
+*   @n : integer pointer
+*   Return: Void
+*/
+void reset_to_98(int *n);
+
+void reset_to_98(int *n)
+{
+	*(n) = 98;
+}
