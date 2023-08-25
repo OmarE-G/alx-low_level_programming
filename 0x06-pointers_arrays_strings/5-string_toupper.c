2b98e99736276beaf9b8b6beed8b24a0aafd2a52
Omar Essam <omar161157@eng.psu.edu.eg>
2023-08-25 05:35:17 +0300

    new commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * string_toupper - uppercase
+ *
+ * @s : string
+ *
+ *Return: string in uppercase
+ */
+char *string_toupper(char *s)
+{
+	char *st = s;
+
+	while (*st)
+		*st == *st > 'a' ? *st - 'a' + 'A' : *st, st++;
+
+	return (s);
+}
+
