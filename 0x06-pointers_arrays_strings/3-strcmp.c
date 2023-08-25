3d3f6b3213f46fc9b31a731da1e91b0f643c72bc
Omar Essam <omar161157@eng.psu.edu.eg>
2023-08-25 03:51:52 +0300

    new commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,7 +16,13 @@ int _strcmp(char *s1, char *s2)
 			break;
 		s1++, s2++;
 	}
-	return ((*s1 > *s2) ? 1 : ((*s2 > *s1) ? -1 : 0));
+
+	if (*s1 == *s2)
+		return (0);
+	if (*s1 > *s2)
+		return (1);
+
+	return (-1);
 }
 
 
