c615b8fca0adb13af867a74cea29fbc7879a1ed1
Omar Essam <omar161157@eng.psu.edu.eg>
2023-08-25 03:22:57 +0300

    new commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * _strncoy - concat strings
+ *
+ * @dest : d
+ * @src : s
+ * @n : sz
+ * Return: char*
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *st = dest;
+	int c = 0;
+
+	while (c < n && *src != '\0')
+		*st = *src, st++, src++, c++;
+
+	*st = '\0';
+
+	return (dest);
+}
+
+
