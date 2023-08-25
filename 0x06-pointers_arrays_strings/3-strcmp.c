ef22290713a192acaec35d5539c1008648da22c6
Omar Essam <omar161157@eng.psu.edu.eg>
2023-08-25 03:43:02 +0300

    new commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * _strcmp - concat strings
+ *
+ * @s1 : d
+ * @s2 : s
+ * Return: int*
+ */
+int _strcmp(char *s1, char *s2)
+{
+
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+			break;
+		s1++, s2++;
+	}
+	return (*s1 > *s2 ? 1 : s2 > s1 ? -1 : 0);
+}
+
+
