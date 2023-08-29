7a7c0b109bc96f280abeb6ae86759b7c7488aac5
Omar Essam <omar161157@eng.psu.edu.eg>
2023-08-29 19:09:38 +0300

    new commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<string.h>
+
+/**
+ * _memcpy - sets memory
+ *
+ * @dest: st
+ * @src: val
+ * @n: count
+ * Return: char*
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+	return (memcpy(dest, src, n));
+}
+/**/
