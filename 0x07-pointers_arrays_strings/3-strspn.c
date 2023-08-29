5cf28c61032b0763466ef77ee6d44788a445199d
Omar Essam <omar161157@eng.psu.edu.eg>
2023-08-29 19:13:58 +0300

    new commit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include<string.h>
+
+/**
+ * _strspn - sets memory
+ *
+ * @s: st
+ * @accept: val
+ * Return: char*
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+
+	return (strspn(s, accept));
+}
+/**/
