9e6bfd9cc80933bcdb8791c557ce677a26c183fb
Omar Essam <omar161157@eng.psu.edu.eg>
2023-08-29 04:46:23 +0300

    new commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * _memset - sets memory
+ *
+ * @s: st
+ * @b: val
+ * @n: count
+ * Return: char*
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int c = 0;
+
+	while (c < n)
+		s[c] = b, c++;
+
+	return (s);
+}
