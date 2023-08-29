7c8101c59614d0253b4aa5b8c77d614076a44818
Omar Essam <omar161157@eng.psu.edu.eg>
2023-08-29 19:11:42 +0300

    new commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include<string.h>
+
+/**
+ * _strchr - sets memory
+ *
+ * @s: st
+ * @c: val
+ * Return: char*
+ */
+char *_strchr(char *s, char c)
+{
+
+	return (strchr(s, c));
+}
+/**/
