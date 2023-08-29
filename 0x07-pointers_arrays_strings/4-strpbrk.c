0226713ecbd9dfa619e4e6029ace7ca1c8a38551
Omar Essam <omar161157@eng.psu.edu.eg>
2023-08-29 19:16:28 +0300

    new commit

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include<string.h>
+
+/**
+ * _strpbrk - sets memory
+ *
+ * @s: st
+ * @accept: val
+ * Return: char*
+ */
+char *_strpbrk(char *s, char *accept)
+{
+
+	return (strpbrk(s, accept));
+}
+/**/
