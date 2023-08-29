1a9b98fe9fd0475dc99befc9b66dfadc27a89437
Omar Essam <omar161157@eng.psu.edu.eg>
2023-08-29 20:39:22 +0300

    new commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include<string.h>
+
+/**
+ * _strstr - sets memory
+ *
+ * @haystack: st
+ * @needle: val
+ * Return: char*
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+	return (strstr(haystack, needle));
+}
+/**/
