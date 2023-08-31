11cd81aabb6f1a815ed5c1bd50bbd58ea41e268c
Omar Essam <omar161157@eng.psu.edu.eg>
2023-08-31 04:05:53 +0300

    new commit

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,12 +1,24 @@
 #include<stdio.h>
 
 /**
- * @brief 
- * 
- * 
- * @param n 
+ * _pow_recursion - print power of a number
+ *
+ *
+ * @x: number
+ * @y: power
+ * Return: power of x
  */
-void p(int n)
+int _pow_recursion(int x, int y)
 {
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	return (x * _pow_recursion(x, y - 1));
 
 }
