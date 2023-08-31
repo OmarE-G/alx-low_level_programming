5bd7c56a56ccfa5c4846d245b7726823e39bccee
Omar Essam <omar161157@eng.psu.edu.eg>
2023-08-31 04:20:31 +0300

    new commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,12 +1,46 @@
 #include<stdio.h>
 
-/**
- * @brief 
- * 
- * 
- * @param n 
- */
-void p(int n)
-{
 
+/**
+ * prime - check if a number is prime
+ *
+ *
+ * @n : number
+ * @i : iterator
+ * Return: 1 if prime, 0 if not
+ */
+int prime(int n, int i)
+{
+	if (i == n)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (prime(n, i + 1));
 }
+/**
+ * is_prime_number - check if a number is prime
+ *
+ *
+ * @n : number
+ * Return: 1 if prime, 0 if not
+ */
+int is_prime_number(int n)
+{
+	int i = 2;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (n == 2)
+	{
+		return (1);
+	}
+	return (prime(n, i));
+}
+
+
