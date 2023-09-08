9431715efbc6237f2122ca4d8555b323aa181ad6
Omar Essam <omar161157@eng.psu.edu.eg>
2023-09-08 07:57:04 +0300

    new commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+/**
+* _strdup - makes an array
+*@str: str
+*Return:ptr
+*/
+
+
+char *_strdup(char *str)
+{
+
+	return (strdup(str));
+
+}
