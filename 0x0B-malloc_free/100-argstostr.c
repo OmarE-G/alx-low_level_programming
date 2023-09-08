c6882d4dec50640c32387c9606c3d1a36ab42146
Omar Essam <omar161157@eng.psu.edu.eg>
2023-09-08 08:51:31 +0300

    new commit

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#ifdef __STDC_ALLOC_LIB__
+#define __STDC_WANT_LIB_EXT2__ 1
+#else
+#define _POSIX_C_SOURCE 200809L
+#endif
+#include <string.h>
+/**
+ * argstostr - makes an array
+ *@ac: sz
+ *@av: sz
+ *Return:ptr
+*/
+
+
+char *argstostr(int ac, char **av)
+{
+	int i = 0;
+	char *ptr = NULL;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	while (i < ac)
+	{
+		ptr = realloc(ptr, strlen(ptr) + strlen(av[i]) + 1);
+		if (ptr == NULL)
+			return (NULL);
+		strcat(ptr, av[i]);
+	}
+	return (ptr);
+
+}
