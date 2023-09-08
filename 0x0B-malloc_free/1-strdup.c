ff7a559539732f21cd22847050ed559bb9589336
Omar Essam <omar161157@eng.psu.edu.eg>
2023-09-08 07:58:55 +0300

    new commit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
+#ifdef __STDC_ALLOC_LIB__
+#define __STDC_WANT_LIB_EXT2__ 1
+#else
+#define _POSIX_C_SOURCE 200809L
 #include <string.h>
 /**
 * _strdup - makes an array
