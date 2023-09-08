fa31d07e5d371e1ad4bdc4eaa1a6c3d0a1177aad
Omar Essam <omar161157@eng.psu.edu.eg>
2023-09-08 08:21:33 +0300

    new commit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+#ifdef __STDC_ALLOC_LIB__
+#define __STDC_WANT_LIB_EXT2__ 1
+#else
+#define _POSIX_C_SOURCE 200809L
+#endif
+#include <string.h>
+/**
+ * alloc_grid - makes an array
+ *@width: sz
+ *@height: sz
+ *Return:ptr
+*/
+
+
+int **alloc_grid(int width, int height)
+{
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	int **ptr = (int **)malloc(height * sizeof(int *));
+	int i = 0, j = 0;
+
+	if (ptr == NULL)
+		return (NULL);
+	while (i < height)
+	{
+		ptr[i] = (int *)calloc(width, sizeof(int));
+		if (ptr[i] == NULL)
+		{
+			while (i >= 0)
+			{
+				free(ptr[i]);
+				i--;
+			}
+			free(ptr);
+			return (NULL);
+		}
+		i++;
+	}
+}
