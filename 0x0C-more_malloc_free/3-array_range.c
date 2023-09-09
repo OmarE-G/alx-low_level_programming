3f617fbb3cc51096872f2e9284ed248b5ef39384
Omar Essam <omar161157@eng.psu.edu.eg>
2023-09-09 11:15:32 +0300

    new commit

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+/**
+ * array_range - Write a function that creates an array of integers.
+ * @min: int
+ * @max: int
+ * Return: ptr
+ */
+
+
+int *array_range(int min, int max)
+{
+	int i = 0;
+	int *ptr = malloc(sizeof(int) * (max - min + 1));
+
+	if (min > max)
+		return (NULL);
+
+
+	if (ptr == NULL)
+		return (NULL);
+
+	while (min <= max)
+		ptr[i++] = min++;
+	return (ptr);
+}
