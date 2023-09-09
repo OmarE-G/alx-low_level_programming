ca31601438364265cc00006f00f0683fbc2441b8
Omar Essam <omar161157@eng.psu.edu.eg>
2023-09-09 11:29:36 +0300

    new commit

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+/**
+ *_realloc - Write a function that
+ reallocates a memory block using malloc and free
+ *@ptr: void
+ *@old_size: unsigned int
+ *@new_size: unsigned int
+ *Return: ptr
+ */
+
+
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (ptr == NULL)
+		return (malloc(new_size));
+	if (new_size == old_size)
+		return (ptr);
+	return (realloc(ptr, new_size));
+}
