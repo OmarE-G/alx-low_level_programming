e543acc12df712a1682d33cd907ef444cb7d6a20
Omar Essam <omar161157@eng.psu.edu.eg>
2023-09-14 04:48:20 +0300

    new commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: function pointer
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
