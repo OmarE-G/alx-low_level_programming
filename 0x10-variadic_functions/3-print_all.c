3dd7a9fc229dfb4e5ff7a6680d4e62b0b2f16534
Omar Essam <omar161157@eng.psu.edu.eg>
2023-09-15 05:42:43 +0300

    new commit

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,41 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ * Return: void
+*/
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	unsigned int i = 0;
+	char *s;
+
+	va_start(ap, format);
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, double));
+				break;
+			case 's':
+				s = va_arg(ap, char *);
+				if (s == NULL)
+					s = "(nil)";
+				printf("%s", s);
+				break;
+			default:
+				break;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
+}
