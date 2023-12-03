3f77ba081984af8faf9416254e5ef39df6c67d3e
Omar Essam <omar161157@eng.psu.edu.eg>
2023-12-03 11:08:24 +0200

    new commit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,30 +12,58 @@
  * Return: int
  */
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - sets key to value
+ *
+ * @ht: table
+ * @key: key
+ * @value: value
+ * Return: int
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	int pos = key_index((const unsigned char *) key, ht->size);
 	hash_node_t *new_node, *curr = ht->array[pos];
 
-
-
 	while (curr)
 	{
 		if (strcmp(curr->key, key) == 0)
 		{
-			strcpy(curr->value, value);
+			free(curr->value);
+			curr->value = strdup(value);
+			if (curr->value == NULL)
+				return (0);
 			return (1);
 		}
 		curr = curr->next;
 	}
-	new_node = malloc(sizeof(hash_node_t));
 
-	if (!new_node)
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
 		return (0);
 
-	strcpy(new_node->key, key), strcpy(new_node->value, value);
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
 
 	new_node->next = ht->array[pos];
 	ht->array[pos] = new_node;
+
 	return (1);
 }
