662f4139268920680710017d120ccd702e408990
Omar Essam <omar161157@eng.psu.edu.eg>
2023-12-03 11:48:01 +0200

    new commit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,6 +4,37 @@
 #include "hash_tables.h"
 
 
+
+/**
+ * make_hash_node - creates a new hash node
+ * @key: key for the node
+ * @value: for the node
+ *
+ * Return: the new node, or NULL on failure
+ */
+hash_node_t *make_hash_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+	node->next = NULL;
+	return (node);
+}
 /**
  * hash_table_set -sets key to value
  *
@@ -27,31 +58,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(curr->key, key) == 0)
 		{
 			newval = strdup(value);
+
 			if (newval == NULL)
 				return (0);
+
 			free(curr->value);
 			curr->value = newval;
 			return (1);
 		}
 		curr = curr->next;
 	}
-	new_node = malloc(sizeof(hash_node_t));
+
+	new_node = make_hash_node(key, value);
+
 	if (new_node == NULL)
-		return (0);
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
+		return  (0);
+
 	new_node->next = ht->array[pos];
 	ht->array[pos] = new_node;
+
 	return (1);
 }
