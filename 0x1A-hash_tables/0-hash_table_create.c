4dd3066e3de0c85b526b52de1c738b326cf5ac54
Omar Essam <omar161157@eng.psu.edu.eg>
2023-12-03 09:47:36 +0200

    new commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht->array = malloc(sizeof(hash_node_t) * size);
 
 	if (ht->array == NULL)
-		return NULL;
+		return (NULL);
 
 	return (ht);
 }
