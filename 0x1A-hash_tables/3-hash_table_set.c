bb1cb79c7922b91336665c29bd38fe3fd508f944
Omar Essam <omar161157@eng.psu.edu.eg>
2023-12-03 10:56:32 +0200

    new commit

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - sets key to value
+ *
+ * @ht: table
+ * @key: key
+ * @value: value
+ * Return: int
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	int pos = key_index((const unsigned char *) key, ht->size);
+	hash_node_t *new_node = malloc(sizeof(hash_node_t)), *curr = ht->array[pos];
+
+	if (!new_node)
+		return (0);
+
+	strcpy(new_node->key, key), strcpy(new_node->value, value);
+
+	while (curr)
+	{
+		if (strcmp(curr->key, key) == 0)
+		{
+			strcpy(curr->value, value);
+			break;
+		}
+		curr = curr->next;
+	}
+	new_node->next = ht->array[pos];
+	ht->array[pos] = new_node;
+	return (1);
+}
