6a0d62884c0a57022fae7c30f9d95335786cf5d6
Omar Essam <omar161157@eng.psu.edu.eg>
2023-12-03 12:04:38 +0200

    new commit

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get value
+ *
+ * @ht: table
+ * @key: key
+ * Return: char*
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	int pos;
+	hash_node_t *curr;
+
+	if (!key)
+		return (0);
+
+	pos = key_index((const unsigned char *)key, ht->size);
+	curr = ht->array[pos];
+
+	while (curr)
+	{
+		if (strcmp(key, curr->key) == 0)
+		{
+			return (curr->value);
+		}
+		curr = curr->next;
+	}
+	return (NULL);
+}
