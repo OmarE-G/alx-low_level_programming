8a9dc8b70c4b6f2066d364cdb0d497e5c2e15be8
Omar Essam <omar161157@eng.psu.edu.eg>
2023-12-03 10:21:54 +0200

    new commit

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "hash_tables.h"
+
+
+/**
+ * key_index - Computes the index for a given key.
+ *
+ * @key: Pointer to the key (string) for which to compute the index.
+ * @size: Size of the array or hash table where the index is needed.
+ *
+ * Return: The computed index for the given key.
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
