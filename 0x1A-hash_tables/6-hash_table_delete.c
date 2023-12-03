8b41b127cafe34db59f43d960c0e3bdc383cd164
Omar Essam <omar161157@eng.psu.edu.eg>
2023-12-03 12:58:45 +0200

    new commit

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - delete table
+ *
+ * @ht: table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int pos;
+	hash_node_t *curr, *temp;
+
+	if (!ht)
+		return;
+
+	pos = 0;
+	while (pos++ < ht->size)
+	{
+		curr = ht->array[pos];
+		while (curr)
+		{
+			temp = curr;
+			if (temp->key != NULL)
+			{
+				free(temp->value);
+				free(temp->value);
+			}
+			curr = temp->next;
+			free(temp);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
